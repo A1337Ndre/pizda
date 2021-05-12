fbf78bcba84d245d55b4c36ae49892ba2aa70688
A1337Ndre <doronin357951@gmail.com>
2021-05-13 01:06:04 +0300

    my pidarskiy commit

---
+++ syka.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function syka() {
+    return (
+        <div>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+        </div>
+    )
+}
+
+export default syka
